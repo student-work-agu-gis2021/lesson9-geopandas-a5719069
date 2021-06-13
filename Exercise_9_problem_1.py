58512439dabd06184adf66b0e1cc0b26933e2da6
a5719069 <82347542+a5719069@users.noreply.github.com>
2021-06-13 22:56:14 +0000

    I have finished problem 1.

--- Exercise_9_problem_1.py
+++ Exercise_9_problem_1.py
@@ -28,11 +28,18 @@ latitudes = [63.748023986816406, 62.90789794921875, 60.511383056640625, 60.44499
 
 # Create a list of coordinate pairs
 # YOUR CODE HERE 1 
-coordpairs=list(zip(longitudes,latitudes))
+coordpairs=None
+coordpairs = []
+for i in range(len(longitudes)):
+  coordpairs.append([longitudes[i], latitudes[i]])
+
+
 
 # Create a shapely Polygon using the 'coordpairs' -list
 # YOUR CODE HERE 2 
-poly=Polygon(coordpairs)
+poly = None
+
+poly = Polygon(coordpairs)
 
 # CODE FOR TESTING YOUR SOLUTION
 
@@ -49,8 +56,9 @@ print(poly.geom_type)
 # Create an empty GeoDataFrame
 geo = None
 # YOUR CODE HERE 3
-geo=gpd.GeoDataFrame(index=[0],columns=["geometry"])
-geo["geometry"]=poly
+geo = gpd.GeoDataFrame()
+geo = gpd.GeoDataFrame(index = [0], columns = ['geometry'])
+geo['geometry'] = poly
 
 # CODE FOR TESTING YOUR SOLUTION
 
@@ -68,10 +76,13 @@ print(len(geo))
 
 # Plot the polygon. What shape is it :) ?
 # YOUR CODE HERE 4
+
 geo.plot()
-plt.show()
+#plt.show()
 
 # What can you see on the map? :) 
+
+# It looks like 
 
 # - save the GeoDataFrame into a Shapefile called `'polygon.shp'`. 
 
@@ -80,6 +91,7 @@ fp = 'polygon.shp'
 
 # YOUR CODE HERE 5
 geo.to_file(fp)
+
 # CODE FOR TESTING YOUR SOLUTION
 
 #Check if output file exists
@@ -100,4 +112,3 @@ def func3():
 
 def func4():
     return geo
-
